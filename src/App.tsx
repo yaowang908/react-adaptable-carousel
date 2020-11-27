e1188eeffd26882e09a2edb426beec78c8f221e8
Yao <yao422321065@yahoo.com>
2020-11-27 00:06:42 -0500

    dont need new publish command

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import Carousel from './components/Carousel';
+import CarouselFullWidth from './components/CarouselFullWidth';
 
 function App() {
   return (
-    <Carousel>
-      <div>
-          <img src="http://placekitten.com/200/300" alt="cat"/>
-      </div>
-      <div>
-          <img src="http://placekitten.com/200/300" alt="cat"/>
-      </div>
-      <div>
-          <img src="http://placekitten.com/200/300" alt="cat"/>
-      </div>
-      <div>
-          <img src="http://placekitten.com/200/300" alt="cat"/>
-      </div>
-    </Carousel>
+    <CarouselFullWidth
+      imgUrlArray={
+        [
+          { imgUrl: "https://via.placeholder.com/200x300.png?text=0", link: 'https://placeholder.com'},
+          { imgUrl: "https://via.placeholder.com/400x300.png?text=1", link: 'https://placeholder.com'},
+          { imgUrl: "https://via.placeholder.com/300x300.png?text=2", link: 'https://placeholder.com'},
+          { imgUrl: "https://via.placeholder.com/200x300.png?text=3", link: 'https://placeholder.com'},
+          { imgUrl: "https://via.placeholder.com/400x300.png?text=4", link: 'https://placeholder.com'},
+          { imgUrl: "https://via.placeholder.com/300x300.png?text=5", link: 'https://placeholder.com'},
+          { imgUrl: "https://via.placeholder.com/200x300.png?text=6", link: 'https://placeholder.com'},
+          { imgUrl: "https://via.placeholder.com/400x300.png?text=7", link: 'https://placeholder.com'},
+          { imgUrl: "https://via.placeholder.com/300x300.png?text=8", link: 'https://placeholder.com'},
+        ]
+      }
+      componentHeight={ 400 }
+      isDivElement={false}
+    ></CarouselFullWidth>
   );
 }
 
