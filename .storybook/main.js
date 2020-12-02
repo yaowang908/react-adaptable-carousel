76b9d9fe3947a419372fab6a91c1e5d941219a5e
Yao <yao422321065@yahoo.com>
2020-12-02 16:11:11 -0500

    bring back storybook after CRA eject

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,31 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+  "stories": [ "../src/**/*.stories.(tsx|mdx)" ],
+  webpackFinal: async config => {
+        config.module.rules.push({
+          test: /\.(ts|tsx)$/,
+          include: path.resolve(__dirname, "../src/components"),
+          use: [
+              // There is also a second option in documentation to use babel instead of ts-loader which should also work
+              {
+                loader: require.resolve('ts-loader'),
+              },
+              {
+                  loader: require.resolve('react-docgen-typescript-loader'),
+                  options: {
+                    tsconfigPath: path.resolve(__dirname, "../tsconfig.json")
+                  }
+              }
+          ],
+        })
+        config.resolve.extensions.push('.ts', '.tsx');
+        return config
+      },
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    '@storybook/addon-docs'
+    // "@storybook/addon-links",
+    // "@storybook/addon-essentials",
+    // "@storybook/preset-create-react-app"
   ]
 }
